9e88acfb4dfa0dc8b2f9a7c0e1ab64b75bd9e29d
Robert Borzyszkowski <robbor@gazeta.pl>
2020-02-23 12:14:38 +0100

    Dodanie metody toString

--- src/main/java/ListaImplementacja.java
+++ src/main/java/ListaImplementacja.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ListaImplementacja implements Lista {
 
     private int [] liczby;
@@ -11,7 +13,25 @@ public class ListaImplementacja implements Lista {
         this.liczby = new int[pojemnosc];
 }
 
-    public void dodajElement(int liczba) {
+//    public void dodajElement(int liczba) {
+//        if (this. rozmiar == this.pojemnosc){
+//            System.out.println("Tablica jest pełna");
+//            this.liczby = Arrays.copyOf(this.liczby, this.liczby.length*2);
+//            this.rozmiar = this.liczby.length;
+//        }
+//        else {
+//            this.liczby[rozmiar] = liczba;
+//            rozmiar++;
+public void dodajElement(int liczba) {
+    if (this.liczby.length > rozmiar) {
+        this.liczby[rozmiar] = liczba;
+        rozmiar++;
+    }
+    else {
+        this.liczby = Arrays.copyOf(liczby,liczby.length + 10);
+        this.liczby[rozmiar] = liczba;
+        rozmiar++;
+        }
 
     }
 
@@ -20,8 +40,14 @@ public class ListaImplementacja implements Lista {
     }
 
     public String pisz() {
-        return null;
+        return "liczby " + Arrays.toString(liczby);
+
     }
+//    public String toString(){
+//        return "Test";
+//    }
+
+
 
     public boolean usunPierwszy(int liczba) {
         return false;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,51 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static java.util.Arrays.*;
 
 public class Main {
     public static void main(String[] args) {
 
+
         Lista lista = new ListaImplementacja();
-        Lista lista2 = new ListaImplementacja(11);
+        Lista lista2 = new ListaImplementacja(10);
 
-   lista.dodajElement(212);
+        lista.dodajElement(201);
+        lista.dodajElement(202);
+        lista.dodajElement(203);
+        lista.dodajElement(204);
+        lista.dodajElement(205);
+        lista.dodajElement(206);
+        lista.dodajElement(207);
+        lista.dodajElement(208);
+        lista.dodajElement(209);
+        lista.dodajElement(210);
+        lista.dodajElement(211);
+        lista.dodajElement(212);
+        lista.dodajElement(213);
+        lista.dodajElement(214);
+        lista.dodajElement(215);
+        lista.dodajElement(216);
+        lista.dodajElement(217);
+        lista.dodajElement(218);
+        lista.dodajElement(219);
+        lista.dodajElement(220);
+        lista.dodajElement(221);
+        lista.dodajElement(222);
+        lista.dodajElement(223);
+        lista.dodajElement(224);
+        System.out.println(lista.pisz());
+
+//   int tab[] = new int[5];
+//   tab [0] = 2;
+//   tab [1] = 3;
+//   tab [2] = 4;
+//
+//        System.out.println(tab.length);
+//   tab = Arrays.copyOf(tab, tab.length*2);
+//        System.out.println(tab.length);
+//    }
+
     }
-
 }
